dd23e358ce210e5ae868be6709e753aac2cc776c
Michał Wysoczański <michalwy@gmail.com>
2015-02-05 12:11:34 +0100

    Key bindings tweaks

--- .emacs.d/settings/kbd-settings.el
+++ .emacs.d/settings/kbd-settings.el
@@ -1,13 +1,13 @@
 (global-set-key (kbd "TAB") 'self-insert-command)
 (global-set-key (kbd "DEL") 'backward-delete-char)
 
+(global-set-key (kbd "C-c C-d") 'kill-whole-line)
+(global-set-key (kbd "C-c d") 'kill-whole-line)
+
 (global-set-key (kbd "<C-end>") 'end-of-buffer)
 (global-set-key (kbd "<C-home>") 'beginning-of-buffer)
 
 (global-set-key [f4] 'goto-line)
-
-(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
-(global-set-key (kbd "C-x b") 'ibuffer)
 
 (global-set-key (kbd "<C-S-up>") 'buf-move-up)
 (global-set-key (kbd "<C-S-down>") 'buf-move-down)
@@ -18,6 +18,10 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (global-set-key (kbd "C-x C-z") 'magit-status)
+
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+(global-set-key (kbd "C-x b") 'ibuffer)
+(global-set-key (kbd "C-x f") 'ido-find-file)
 
 (global-set-key (kbd "C-x C-/") 'goto-last-change)
 
